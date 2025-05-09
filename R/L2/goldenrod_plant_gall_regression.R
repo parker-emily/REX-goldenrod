6f223fa7e117f0713633ab9032d85cbcf4368da8
Emily Parker <118372614+parker-emily@users.noreply.github.com>
2025-05-09 16:49:39 -0700

    add chamber data

--- R/L2/goldenrod_plant_gall_regression.R
+++ R/L2/goldenrod_plant_gall_regression.R
@@ -10,6 +10,29 @@ dir <- setwd("C:/Users/parkere5/Documents/Goldenrod Data")
 height <- read.csv("T7_warmx_soca_height_harvest_L1.csv")
 gall <- read.csv("T7_warmx_Soca_galls_L1.csv")
 biomass <- read.csv("T7_warmx_soca_biomass_L1.csv")
+chmb_vol <- read.csv("T7_warmx_Soca_gall_chmb_vol_L1.csv")
+chmb_count <- read.csv("T7_warmx_Soca_gall_chmb_count_L1.csv")
+
+
+#rename chamber columns
+chmb_vol <- chmb_vol %>% 
+  rename("Unique_ID" = "unique_plant_number",
+         "Rep" = "rep",
+         "Footprint" = "footprint",
+         "Climate_Treatment" = "treatment")
+
+chmb_count <- chmb_count %>% 
+  rename("Unique_ID" = "unique_plant_number",
+         "Rep" = "rep",
+         "Footprint" = "footprint",
+         "Climate_Treatment" = "treatment")
+
+
+
+#merge gall characters
+gall <- merge(gall, chmb_count, by = c("Unique_ID", "Rep", "Footprint", "Climate_Treatment"))
+gall <- merge(gall, chmb_vol, by = c("Unique_ID", "Rep", "Footprint", "Climate_Treatment"))
+
 
 clean <- merge(height, gall, by = c("Unique_ID", "Treatment", "Rep", "Footprint", "Subplot", "Climate_Treatment", "Year"))
 clean <- clean %>%
@@ -50,6 +73,25 @@ height_year <-
         axis.text.y = element_text(size = 14),
         axis.title = element_text(size=14,face="bold"))
 
+height_vol <-
+  ggplot(clean, aes(x=Height_cm, y= chamber_volume_mm3)) +
+  geom_point(shape = 21, size = 3, color = "black",fill = "purple4") + 
+  geom_smooth(method = lm, se = FALSE, color = "black") +
+  labs(x="Stem height (cm)", y = "Chamber volume (mm³)") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 11),
+        axis.text.y = element_text(size = 14),
+        axis.title = element_text(size=14,face="bold"))
+
+height_count <-
+  ggplot(clean, aes(x=Height_cm, y= num_of_chambers)) +
+  geom_point(shape = 21, size = 3, color = "black",fill = "purple4") + 
+  geom_smooth(method = lm, se = FALSE, color = "black") +
+  labs(x="Stem height (cm)", y = "Number of chambers") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 11),
+        axis.text.y = element_text(size = 14),
+        axis.title = element_text(size=14,face="bold"))
 
 bm_plot <- 
   ggplot(clean, aes(x=Biomass, y= Dried_Weight)) +
@@ -63,7 +105,7 @@ bm_plot <-
         axis.title = element_text(size=14,face="bold"))
 
 bm_treat <-
-ggplot(clean, aes(x=Height_cm, y= Dried_Weight)) +
+ggplot(clean, aes(x=Biomass, y= Dried_Weight)) +
   geom_point(shape = 21, size = 3, color = "black",fill = "purple4") + 
   geom_smooth(method = lm, se = FALSE, color = "black") +
   labs(x="Stem biomass (g)", y = "Dried gall biomass (g)") +
@@ -74,11 +116,31 @@ ggplot(clean, aes(x=Height_cm, y= Dried_Weight)) +
         axis.title = element_text(size=14,face="bold"))
 
 bm_year <-
-  ggplot(clean, aes(x=Height_cm, y= Dried_Weight)) +
+  ggplot(clean, aes(x=Biomass, y= Dried_Weight)) +
   geom_point(shape = 21, size = 3, color = "black",fill = "purple4") + 
   geom_smooth(method = lm, se = FALSE, color = "black") +
   labs(x="Stem biomass (g)", y = "Dried gall biomass (g)") +
   facet_wrap(~Year) +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 11),
+        axis.text.y = element_text(size = 14),
+        axis.title = element_text(size=14,face="bold"))
+
+bm_vol <-
+  ggplot(clean, aes(x=Biomass, y= chamber_volume_mm3)) +
+  geom_point(shape = 21, size = 3, color = "black",fill = "purple4") + 
+  geom_smooth(method = lm, se = FALSE, color = "black") +
+  labs(x="Stem biomass (g)", y = "Chamber volume (mm³)") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 11),
+        axis.text.y = element_text(size = 14),
+        axis.title = element_text(size=14,face="bold"))
+
+bm_count <-
+  ggplot(clean, aes(x=Biomass, y= num_of_chambers)) +
+  geom_point(shape = 21, size = 3, color = "black",fill = "purple4") + 
+  geom_smooth(method = lm, se = FALSE, color = "black") +
+  labs(x="Stem biomass (g)", y = "Number of chambers") +
   theme_bw() +
   theme(axis.text.x = element_text(size = 11),
         axis.text.y = element_text(size = 14),
@@ -101,7 +163,15 @@ ggarrange(height_year, bm_year,
           nrow = 1, common.legend = T, legend="right",widths = c(1, 1))
 dev.off()
 
+png("gall_regression_vol.png", units="in", width=11, height=5, res=300)
+ggarrange(height_vol, bm_vol,
+          nrow = 1, common.legend = T, legend="right",widths = c(1, 1))
+dev.off()
 
+png("gall_regression_count.png", units="in", width=11, height=5, res=300)
+ggarrange(height_count, bm_count,
+          nrow = 1, common.legend = T, legend="right",widths = c(1, 1))
+dev.off()
 
 ##stats
 height_lm <- lm(Dried_Weight ~ Height_cm, data = clean)
